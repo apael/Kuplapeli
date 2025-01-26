41d8dc56fead232bd20ab29bdd3c6e5729e54a88
Arttu <Arttu.Myllari@edu.savonia.fi>
2025-01-26 02:29:34 +0200

    Story button+story

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -3,9 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    public GameObject canvasToHide;
+
     // Optional: Overload method to dynamically set the scene name and load it
     public void ChangeScene(string targetSceneName)
     {
         SceneManager.LoadScene(targetSceneName);
+    }
+    public void OnStoryButtonClick()
+    {
+        // Check if the canvas is not already null
+        if (canvasToHide != null)
+        {
+            canvasToHide.SetActive(false); // Hide the canvas
+            Debug.Log("Canvas Hidden");
+        }
+    }
+    public void OnBackButtonClick()
+    {
+        // Check if the canvas is not already null
+        if (canvasToHide != null)
+        {
+            canvasToHide.SetActive(true); // Hide the canvas
+            Debug.Log("Canvas shown");
+        }
     }
 }
